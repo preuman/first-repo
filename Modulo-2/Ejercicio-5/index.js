4f5d3d576b2b978125e298c3e2409c2cd46f3293
David Gonzalez Fernandez <david.gonzalezfernandez@fujitsu.com>
2022-11-19 21:04:09 +0100

    add modulo2 ejercicio5

--- Modulo-2/Ejercicio-5/index.js
+++ Modulo-2/Ejercicio-5/index.js
@@ -1,4 +1,31 @@
 
-const dobleNumero = numero => numero * 2;
+const personas = [
+    { nombre: 'Juan', edad: 18 },
+    { nombre: 'María', edad: 16 },
+    { nombre: 'Pedro', edad: 20 },
+    { nombre: 'Pablo', edad: 15 },
+    { nombre: 'Laura', edad: 19 },
+];
 
-console.log(dobleNumero(2));
+function personasMayorEdad (lista)  {
+    let listaNueva = [];
+    let j = 0;
+    for (let i=0 ; i<5 ; i++) {
+        if (lista[i].edad >= 18) {
+        listaNueva[j] = lista[i];
+        j++;
+        }
+    }
+
+    return listaNueva;
+}
+
+const mayoresDeEdad = personasMayorEdad(personas);
+
+console.log(mayoresDeEdad);
+
+
+
+let edad = Math.min(personas);
+
+console.log (edad);
