7aaaf74f8dae4f8077c37f6006a02152cba97313
David Gonzalez Fernandez <david.gonzalezfernandez@fujitsu.com>
2022-11-19 20:44:33 +0100

    add modulo2 ejercicio4

--- Modulo-2/Ejercicio-4/index.js
+++ Modulo-2/Ejercicio-4/index.js
@@ -1,4 +1,12 @@
 
-const dobleNumero = numero => numero * 2;
+const parOImpar = (numero) => {
+    if (numero % 2 == 0) {
+        return "par";
+    } else {
+        return "impar";
+    }
+}
 
-console.log(dobleNumero(2));
+console.log(parOImpar(2));
+
+console.log(parOImpar(5));
