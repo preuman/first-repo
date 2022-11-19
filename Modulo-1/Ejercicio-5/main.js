864f809f38d8622bc3e83cfc14f54c605b0e8e60
David Gonzalez Fernandez <david.gonzalezfernandez@fujitsu.com>
2022-11-19 20:04:33 +0100

    add ejercicio 5

---
+++ Modulo-1/Ejercicio-5/main.js
@@ -0,0 +1,11 @@
+let pokemons = ['Picachu' , 'Charizar', 'Miutu'];
+
+console.log(pokemons[0]);
+console.log(pokemons[1]);
+console.log(pokemons[2]);
+
+console.log(pokemons[0] + '\n' + pokemons[1] + '\n' + pokemons[2]);
+
+for (i=0 ; i < pokemons.length; i++) {
+    console.log(pokemons[i]);
+}
