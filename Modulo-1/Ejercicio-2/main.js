7a48b4c290961d832aee04bcb125753c5a310141
David Gonzalez Fernandez <david.gonzalezfernandez@fujitsu.com>
2022-11-19 19:50:32 +0100

    add ejercicio 2

--- Modulo-1/Ejercicio-2/main.js
+++ Modulo-1/Ejercicio-2/main.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo");
+let nombre = "David";
+let edad = 41;
+let mayorDeEdad = true;
+let direccion = {
+    calle: 'Flor de Adelfa' ,
+    numero: 4 ,
+    piso: 2 ,
+    puerta: 'C' ,
+    codigoPostal: 41020 ,
+    ciudad: 'Sevilla' ,
+    provincia: 'Sevilla' 
+};
+let coloresFavoritos = ['verde' , 'azul', 'marron'];
+let lenguajesFavoritos = ['JavaScript', 'Cobol'];
+
+let mejorLenguaje = lenguajesFavoritos[0];
+let peorLenguaje = lenguajesFavoritos[1];
