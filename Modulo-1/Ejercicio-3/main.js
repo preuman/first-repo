63bf8dd379854d38efe1aa114d82dde2fce827d8
David Gonzalez Fernandez <david.gonzalezfernandez@fujitsu.com>
2022-11-19 19:56:13 +0100

    add ejercicio 3

---
+++ Modulo-1/Ejercicio-3/main.js
@@ -0,0 +1,12 @@
+let numero1 = 1;
+let numero2 = 2;
+
+console.log(numero1+numero2);
+
+let numero3 = 3;
+
+console.log(`Resta: ` + (numero3 - numero2));
+
+console.log(`Multiplicacion: ` + (numero3 * numero2));
+
+console.log(`Division: ` + (numero3 / numero2));
