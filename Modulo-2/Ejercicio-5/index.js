790e09bfe443d6624dcffb5b30520cb217294026
David Gonzalez Fernandez <david.gonzalezfernandez@fujitsu.com>
2022-11-20 20:31:47 +0100

    add modulo2 ejercicio6

--- Modulo-2/Ejercicio-5/index.js
+++ Modulo-2/Ejercicio-5/index.js
@@ -25,7 +25,34 @@ const mayoresDeEdad = personasMayorEdad(personas);
 console.log(mayoresDeEdad);
 
 
+function personaMasJoven (lista) {
+    let nombreMenor;
+    let edadMenor = 99;
 
-let edad = Math.min(personas);
+    for (let i=0 ; i < 5 ; i++) {
+        if (lista[i].edad < edadMenor) {
+            nombreMenor = lista[i].nombre;
+            edadMenor = lista[i].edad;
+        }
+    }
 
-console.log (edad);
+    return nombreMenor;
+}
+
+function personaMasVieja (lista) {
+    let nombreMayor;
+    let edadMayor = 0;
+
+    for (let i=0 ; i < 5 ; i++) {
+        if (lista[i].edad > edadMayor) {
+            nombreMayor = lista[i].nombre;
+            edadMayor = lista[i].edad;
+        }
+    }
+
+    return nombreMayor;
+}
+
+console.log(personaMasJoven(personas));
+
+console.log(personaMasVieja(personas));
